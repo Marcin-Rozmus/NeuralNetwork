9b7663f0b31b1f38408fb0fc136e1513bf9838eb
Marcin Rozmus <rozmus.marcin@outlook.com>
2023-07-19 18:28:28 +0200

    Single neuron code

---
+++ SingleNeuron.py
@@ -0,0 +1,11 @@
+inputs = [1, 2, 3]
+
+weights = [1.0, 0.5, -0.2]
+bias = 1.5
+
+output = [weights[0] * inputs[0] +
+          weights[1] * inputs[1] +
+          weights[2] * inputs[2] +
+          bias]
+
+print(output)
